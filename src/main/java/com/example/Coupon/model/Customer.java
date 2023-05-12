8d21e1e1b43f42a817727d4a0ba1ff33d0be7984
MICHAL  COHEN <mc21322132@gmail.com>
2023-05-12 11:00:53 +0300

    Initial commit

---
+++ src/main/java/com/example/Coupon/model/Customer.java
@@ -0,0 +1,75 @@
+package com.example.Coupon.model;
+
+import jakarta.persistence.Entity;
+
+import java.util.List;
+@Entity
+public class Customer {
+    private int customer_id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private List<Coupon> coupons;
+
+    public String toString() {
+        return "Customer{customer_id=" + this.customer_id + ", firstName='" + this.firstName + "', lastName='" + this.lastName + "', email='" + this.email + "', password='" + this.password + "', coupons=" + this.coupons + "}";
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCoupons(List<Coupon> coupons) {
+        this.coupons = coupons;
+    }
+
+    public int getCustomer_id() {
+        return this.customer_id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public List<Coupon> getCoupons() {
+        return this.coupons;
+    }
+
+    public Customer(int customer_id, String firstName, String lastName, String email, String password, List<Coupon> coupons) {
+        this.customer_id = customer_id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.coupons = coupons;
+    }
+}
